b3bd444528cf3848cbdb3b0e414fa7b85359de1c
luke <lukevance90@gmail.com>
2019-09-02 17:06:26 -0600

    updated configs

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Fantasy data is live baby!',
         input: event,
       },
       null,
